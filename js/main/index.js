16b7a78a8e7fbdf66569a337b173389498a1a345
jewel <414608192@qq.com>
2016-07-17 23:11:39 +0800

    解决chrome和safari按钮点击状态不兼容问题

--- js/main/index.js
+++ js/main/index.js
@@ -7,36 +7,44 @@ $(function(){
         $('.preBtn i').css("visibility","visible");
     },function() {
         $('.preBtn i').css("visibility","hidden");
+        $('.preBtn').css("background-color","transparent");
+
     }).trigger("mouseleave");
 
     $('.nextBtn').hover(function(){
         $('.nextBtn i').css("visibility","visible");
     },function() {
         $('.nextBtn i').css("visibility","hidden");
+        $('.nextBtn').css("background-color","transparent");
+
     }).trigger("mouseleave");
 
     $('.preBtnOfTeams').hover(function(){
         $('.preBtnOfTeams i').css("visibility","visible");
     },function() {
         $('.preBtnOfTeams i').css("visibility","hidden");
+        $('.preBtnOfTeams').css("background-color","transparent");
     }).trigger("mouseleave");
 
     $('.nextBtnOfTeams').hover(function(){
         $('.nextBtnOfTeams i').css("visibility","visible");
     },function() {
         $('.nextBtnOfTeams i').css("visibility","hidden");
+        $('.nextBtnOfTeams').css("background-color","transparent");
     }).trigger("mouseleave");
 
     $('.preBtnOfProject').hover(function(){
         $('.preBtnOfProject i').css("visibility","visible");
     },function() {
         $('.preBtnOfProject i').css("visibility","hidden");
+        $('.preBtnOfProject').css("background-color","transparent");
     }).trigger("mouseleave");
 
     $('.nextBtnOfProject').hover(function(){
         $('.nextBtnOfProject i').css("visibility","visible");
     },function() {
         $('.nextBtnOfProject i').css("visibility","hidden");
+        $('.nextBtnOfProject').css("background-color","transparent");
     }).trigger("mouseleave");
 
 
