fc0a23fc720251af25e7479e82d1d45388852a24
mingjiezhang <zhangmjtim@qq.com>
2016-07-19 11:26:33 +0800

    update find_return

--- js/main/find_return.js
+++ js/main/find_return.js
@@ -42,7 +42,39 @@ $('#pwd').blur(function () {
     }
 }); //密码判断；
 $("#finish").click(function(){
+    var credential=document.getElementById("hash_id");
+    var pwd = document.getElementById('pwd').value;
+    var pwd_hash = hex_sha1("pwd");
     if(pwd_falg==true){
-
+        var data={account:account,credential:credential,pwd:pwd_hash};
+        $.ajax({
+            type: 'POST',
+            data: data,
+            url: '../../data/find.json',
+            dataType: 'json',
+            success: function (data) {
+                if(data.err==0)
+                {
+$("#return_show").css("display","none");
+                    $("#success_show").css("display","block");
+                }
+                if(data.err==-4)
+                {
+                    document.getElementById('result').innerHTML ="账号不存在";
+                }
+                if(data.err==-7)
+                {
+                    document.getElementById('result').innerHTML ="凭据错误";
+                }
+                if(data.err==-10)
+                {
+                    document.getElementById('result').innerHTML ="操作失败";
+                }
+                if(data.err==-1)
+                {
+                    document.getElementById('result').innerHTML ="请求方法错误";
+                }
+            }
+        });
     }
 })
