58373a67965ee7bf0f2e476c6a976c441e92e0da
mingjiezhang <zhangmjtim@qq.com>
2016-07-22 10:50:57 +0800

    增加登录检测以及跳转url 增加学生个人信息页面

--- js/main/login.js
+++ js/main/login.js
@@ -12,89 +12,102 @@ $(function () {
         }
     }); //选择完角色进入注册页面
     $('#login-conf').click(function () {
-        if (choice == "学生") {
-            var flag = false;
-            var account = document.getElementById("account").value;
-            var pwd = document.getElementById("pwd").value;
-            var pwd_hash = hex_sha1("pwd");
-            var data = {
-                account: account,
-                pwd: pwd_hash
-            };
-            $.ajax({
-                type: 'POST',
-                data: data,
-                url: '../../data/login.json',
-                dataType: 'json',
-                success: function (data) {
-                    var err = data.err;
-                    if (err == 0) {
-                        flag = true;
-                    }
-                    if (err == -1) {
-                        document.getElementById('account-warn').innerHTML = "请求方法错误";
-                    }
-                    if (err == -4) {
-                        document.getElementById('account-warn').innerHTML = "账号不存在";
-                    }
-                    if (err == -5) {
-                        document.getElementById('pwd-warn').innerHTML = "密码错误";
-                    }
-                    if (err == -6) {
-                        document.getElementById('account-warn').innerHTML = "账号未激活";
-                    }
-                    if (err == -10) {
-                        document.getElementById('account-warn').innerHTML = "操作失败";
-                    }
-                    if (flag == true) {
-                        window.location.href = "../studentLogin.html"
-                    }
-                }
-            });
+        var account = document.getElementById("account").value;
+        var pwd = document.getElementById("pwd").value;
+        if (account == "" || pwd == "") {
+            if (account == "") {
+                document.getElementById("account-warn").innerHTML = "账号不能为空";
+            }
+            if (pwd == "") {
+                document.getElementById("pwd-warn").innerHTML = "密码不能为空";
+            }
         }
-        else {
-            var flag = false;
-            var account = document.getElementById("account").value;
-            var pwd = document.getElementById("pwd").value;
-            var pwd_hash = hex_sha1("pwd");
-            var data = {
-                account: account,
-                pwd: pwd_hash
-            };
-            $.ajax({
-                type: 'POST',
-                data: data,
-                url: '../../data/login.json',
-                dataType: 'json',
-                success: function (data) {
-                    var err = data.err;
-                    if (err == 0) {
-                        flag = true;
+        if (account !== "" && pwd !== ""){
+            if (choice == "学生") {
+                var flag = false;
+                var account = document.getElementById("account").value;
+                var pwd = document.getElementById("pwd").value;
+                var pwd_hash = hex_sha1("pwd");
+                var data = {
+                    account: account,
+                    pwd: pwd_hash
+                };
+                $.ajax({
+                    type: 'POST',
+                    data: data,
+                    url: '../../data/login.json',
+                    dataType: 'json',
+                    success: function (data) {
+                        var err = data.err;
+                        if (err == 0) {
+                            flag = true;
+                        }
+                        if (err == -1) {
+                            document.getElementById('account-warn').innerHTML = "请求方法错误";
+                        }
+                        if (err == -4) {
+                            document.getElementById('account-warn').innerHTML = "账号不存在";
+                        }
+                        if (err == -5) {
+                            document.getElementById('pwd-warn').innerHTML = "密码错误";
+                        }
+                        if (err == -6) {
+                            document.getElementById('account-warn').innerHTML = "账号未激活";
+                        }
+                        if (err == -10) {
+                            document.getElementById('account-warn').innerHTML = "操作失败";
+                        }
+                        if (flag == true) {
+                            window.location.href = "../studentLogin.html?account="+account;
+                        }
                     }
-                    if (err == -1) {
-                        document.getElementById('account-warn').innerHTML = "请求方法错误";
+                });
+            }
+            else {
+                var flag = false;
+                var account = document.getElementById("account").value;
+                var pwd = document.getElementById("pwd").value;
+                var pwd_hash = hex_sha1("pwd");
+                var data = {
+                    account: account,
+                    pwd: pwd_hash
+                };
+                $.ajax({
+                    type: 'POST',
+                    data: data,
+                    url: '../../data/login.json',
+                    dataType: 'json',
+                    success: function (data) {
+                        var err = data.err;
+                        if (err == 0) {
+                            flag = true;
+                        }
+                        if (err == -1) {
+                            document.getElementById('account-warn').innerHTML = "请求方法错误";
+                        }
+                        if (err == -10) {
+                            document.getElementById('account-warn').innerHTML = "操作失败";
+                        }
+                        if (err == -4) {
+                            document.getElementById('pwd-warn').innerHTML = "账号或密码错误";
+                        }
+                        if (err == -5) {
+                            document.getElementById('account-warn').innerHTML = "账号不可用";
+                        }
+                        if (flag == true) {
+                            window.location.href = "../pages/indexForMainPage.html?account="+account;
+                        }
                     }
-                    if (err == -10) {
-                        document.getElementById('account-warn').innerHTML = "操作失败";
-                    }
-                    if (err == -4) {
-                        document.getElementById('pwd-warn').innerHTML = "账号或密码错误";
-                    }
-                    if (err == -5) {
-                        document.getElementById('account-warn').innerHTML = "账号不可用";
-                    }
-                    if (flag == true) {
-                        window.location.href = "../pages/indexForMainPage.html"
-                    }
-                }
-            });
-        }
+                });
+            }
+            }
+
 
 
     });
     /*忘记密码跳转*/
     $('#pwd-find').click(function(){
-            window.location.href = "../../main/role/find.html"
+            window.location.href = "../../main/role/find.html";
     });
 
 })
