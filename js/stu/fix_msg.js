491af30cab62703e7283d7209e86ba0718395ce9
yuruilee <2567773985@qq.com>
2016-07-24 21:22:52 +0800

    修改编辑学生信息

--- js/stu/fix_msg.js
+++ js/stu/fix_msg.js
@@ -1,28 +1,48 @@
 $(function () {
-$(".complete_msg").click(function () {//完善个人档案,信息显示在本页面
-     $.ajax({
-        type:"get",
-        url: "complete_msg.html",
-        dataType:"html",
-        success:function(data){
-            $("#student_message_box").html(data);
-        }
-    })
+
+$("#delete_file").click(function () {
+    $(".doc_path").css("opacity","0");
+    //清空之前的缓存的文件的路径
+    var file = $("#update_file");
+    file.after(file.clone().val(""));
+    file.remove();
 });
 
-    
+    for(var i = 1;i<=12 ; i++)
+    {
+        document.getElementById("select_month").innerHTML += '<option value="'+i+'">'+i+'月</option>';
+    }
+
+    for(var i = 1970;i<=2016 ; i++)
+    {
+        document.getElementById("select_year").innerHTML += '<option value="'+i+'">'+i+'年</option>';
+    }
+
 });
 
-function F_Open_dialog(){
+function F_Open_img(){
     document.getElementById("update_photo").click();
+    
+}
+
+function F_Open_file(){
+    document.getElementById("update_file").click();
 }
 
 function show_local_img(file){//预览图片
+    $(".photo_logo").css("opacity","0")
     var img = document.getElementById("local_photo");
     var reader = new FileReader();
     reader.onload = function(evt) {
         img.src = evt.target.result;
     };
     reader.readAsDataURL(file.files[0]);
+}
+
+function show_local_file() {
+    $(".doc_path").css("opacity","1");
+    var path=document.getElementById('update_file');
+    //console.log(path.value.split("\\")[2]);
+    $("#path_line").val(path.value.split("\\")[2]);
 }
 
