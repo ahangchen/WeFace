ce92bc85b8319d9891b17d9596817b1204364480
mingjiezhang <zhangmjtim@qq.com>
2016-08-06 16:14:39 +0800

    main.js的bug修复

--- js/stu/main.js
+++ js/stu/main.js
@@ -433,7 +433,7 @@ $(function () {
         });
 
         /*-----------------------------作品集合展示---------------------------*/
-        edit();       $.ajax({
+    $.ajax({
             type: "POST",
             data: data_file,
             url:cur_site + "student/info/works/get/",
@@ -1959,11 +1959,11 @@ $(function () {
             success: function (data) {
                 var err = data.err;
                 if (err == "0") {
+                    console.log(work_path);
                     work_path = data.path;
-                    $("#work-save").click(works_add_edit());
                     alert("文件上传成功");
                     $("#uploadForm").css("display","none");
-                    console.log(work_path);
+                    $("#work-save").click(works_add_edit());
                 }
                 if (err == "-128") {
                     alert("作品集文件不合法");
