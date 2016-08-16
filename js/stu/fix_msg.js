b444320190ff65f4bc8cef0a87a7953402c17bf3
ahangchen <cweihang@foxmail.com>
2016-08-16 21:05:15 +0800

    学生

--- js/stu/fix_msg.js
+++ js/stu/fix_msg.js
@@ -10,34 +10,38 @@ $(document).ready(function () {
         document.getElementById("select_year").innerHTML += '<option value="' + i + '">' + i + '</option>';
     }
 
-    $("#delete_file").on("click",function(){//删除简历重
+    $("#delete_file").on("click",function(){//删除简历
         $("#file_path").css("display","none");
     });
 
+
     //点击取消
     $("#cancelButton").on("click",function(){
-
+        $('#basic-info').css('display', 'block');
+        $('#student_message_box').css('display', 'none');
     });
 
 
     //点击保存
     $("#saveButton").on("click",function(){
 
-        var newName=$("#first_name").val();
+        var newName=$("#first_name").val()==null?-1:$("#first_name").val();
         var newSex=$("#select_sex").val();
         if(newSex==null)//如果没填性别为0
            newSex=0;
-        var newSchool=$("#school_name").val();
-        var newMajor=$("#major_name").val();
+        var newSchool=$("#school_name").val()==null?-1:$("#school_name").val();
+        var newMajor=$("#major_name").val()==null?-1:$("#major_name").val();
         var newYear=$("#select_year option:selected").text();
         var newMonth=$("#select_month option:selected").text();
         var newLocation=$("#select_location option:selected").text();
-        var newMail=$("#email").val();
-        var newTel=$("#tel").val();
+        var newMail=$("#email").val()==null?-1:$("#email").val();
+        var newTel=$("#tel").val()==null?-1:$("#tel").val();
         if(newYear==null)
             newYear=-1;
         if(newMonth==null)
             newMonth=-1;
+        if(newLocation==null)
+            newLocation=-1;
         //console.log(newYear);
         var result={
             "id": 1/*id*/,
@@ -63,6 +67,25 @@ $(document).ready(function () {
             }
         });
 
+        //显示修改信息
+        $("#detail-name").text(newName);
+        if(newSex==1)
+        $("#detail-sex").text('男');
+        if(newSex==2)
+            $("#detail-sex").text('女');
+        $("#detail-tel").text(newTel);
+        $("#detail-mail").text(newMail);
+        $("#avatar_path").attr('src',cur_media+avatar_path);
+        $("#detail-school").text(newSchool);
+        $("#detail-major").text(newMajor);
+        $("#personal_path").text($("#file_name").val());
+        $("#personal_path").attr('href',path_href);
+        $("#detail-location").text(newLocation);
+
+        $('#basic-info').css('display', 'block');
+        $('#student_message_box').css('display', 'none');
+
+
     });
 
 
@@ -77,6 +100,15 @@ function F_Open_file() {
 }
 
 var avatar_path;
+$.ajax({
+    type: "POST",
+    data: {'id': 1},//data_info,
+    url: cur_site + "student/info/get/",
+    dataType: "json",
+    success: function (data) {
+        avatar_path=data.avatar_path;
+    }
+});
 function show_local_img(file) {//预览图片
     var id=getId();//得到学生的id
     $(".photo_logo").css("opacity", "0");
@@ -105,6 +137,7 @@ function show_local_img(file) {//预览图片
 
 }
 
+var path_href;
 function show_local_file() {
     var id=getId();//得到学生的id
     var path = document.getElementById('update_file');//得到简历的名字
@@ -125,6 +158,7 @@ function show_local_file() {
         dataType: 'json'
     }).done(function(res){
         console.log(res);
+        path_href=res.path;
     })
 
 }
