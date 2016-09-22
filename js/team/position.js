e0e52e72ab58e4b31e1f69ad9a589ce09c35d0f8
yuruilee <yuruilee@icloud.com>
2016-09-22 17:14:29 +0800

    project link

--- js/team/position.js
+++ js/team/position.js
@@ -42,8 +42,10 @@ $(function(){
     // 团队点击职位名字后，在url上获取传职位id
     var tId = getUrlVar('t_id');
 
+
     $('#resumeBtn').attr('href',wefaceBace_site + 'team/resumeManage/resume.html?t_id='+tId);
     $('#positionBtn').attr('href',wefaceBace_site + 'team/position/showPosition.html?t_id='+tId);
+    $("#projectBtn").attr("href",wefaceBace_site+'team/manageProject/manageProject.html?tid='+getId());
 
     $("#addBtn").click(function(){
         $(this).attr('href','addPosition.html?t_id='+tId);
