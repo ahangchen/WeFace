b6a940daaa56bbaefd9529f5fd6ac879d3fd64ca
mingjiezhang <zhangmjtim@qq.com>
2016-09-25 16:02:23 +0800

    -团队消息栏基本完成（链接未添加）

---
+++ js/info/info_team.js
@@ -0,0 +1,47 @@
+(function(){
+        var team_id = location.search.split("=")[1];
+        /*获取团队未读数量*/
+        $.ajax({
+            type: "POST",
+            data:{team_id:team_id,state:'0'},
+            url: cur_site + "team/apply/list/",
+            dataType: "json",
+            success: function (data) {
+                var err = data.err;
+                if (err == '-129') {
+                    console.log("无投递记录");
+                }
+                if (err == '-1') {
+                    console.log("请求方法错误");
+                }
+                if (err == '-10') {
+                    console.log("操作失败");
+                }
+                if (err == '0') {
+                    $('.info-tag').css('display','inline-block').html(data.unread_num);
+                }
+            },
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            }
+        });
+        var user_click=$('#user_click');
+        var list=$('#main-list');
+        $('body').click(function(){
+            list.css('display','none');
+        });
+        //$('#pub-posi').attr('href',url+id);//设置
+        //$('#pub-proj').attr('href',url);//设置
+        user_click.click(function(){
+            if(list.css('display')=='block'){
+                list.css('display','none');
+            }
+            else{
+                list.css('display','block');
+            }
+            event.stopPropagation();
+        });
+        list.click(function(){
+            event.stopPropagation();
+        });
+})();
