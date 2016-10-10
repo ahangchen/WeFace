5e797c46e608e0b03b9284cfa788b208499bd2ca
jewel <414608192@qq.com>
2016-10-10 17:23:20 +0800

    团队处理简历页面的处理

--- js/team/resumeSolve.js
+++ js/team/resumeSolve.js
@@ -1,5 +1,31 @@
 // 简历处理的js
 $(function(){
+    // 获得参数数组
+    function getUrlVars(){
+
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for(var i = 0; i < hashes.length; i++)
+        {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
+    }
+
+    //得到指定参数的value
+    function getUrlVar(name){
+        return getUrlVars()[name];
+    }
+    function getSex(id){
+        if(id == 1){
+            return "男";
+        }
+        else {
+            return "女";
+        }
+    }
     // 从URL中获取投递的id
     var apply_id = getUrlVar('a_id');
     var t_id = getUrlVar('t_id');
@@ -9,7 +35,6 @@ $(function(){
     var wefaceBace_site = "http://110.64.69.101:8080/";
     $('#resumeBtn').attr('href',wefaceBace_site + 'team/resumeManage/resume.html?t_id='+t_id);
     $('#positionBtn').attr('href',wefaceBace_site + 'team/position/showPosition.html?t_id='+t_id);
-
     $("#returnBtn").attr("href","resume.html?t_id="+t_id);
 
     var resumeInfo = {
@@ -31,9 +56,9 @@ $(function(){
         url:cur_site + "team/apply/info/",
         dataType:"json",
         success:function(data){
-            resumeInfo.imgSrc = data.avatar_pah;
+            resumeInfo.imgSrc = cur_media + data.avatar_path;
             resumeInfo.name = data.name;
-            resumeInfo.sex = data.sex;
+            resumeInfo.sex = getSex(data.sex);
             resumeInfo.age = data.age;
             resumeInfo.mail = data.mail;
             resumeInfo.tel = data.tel;
@@ -108,36 +133,32 @@ $(function(){
     $('.resumeName').click(function(){
         window.location.href = resumeInfo.resumePath;
     });
-    // 获得参数数组
-    function getUrlVars(){
 
-        var vars = [], hash;
-        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-        for(var i = 0; i < hashes.length; i++)
-        {
-            hash = hashes[i].split('=');
-            vars.push(hash[0]);
-            vars[hash[0]] = hash[1];
-        }
-        return vars;
-    }
-
-    //得到指定参数的value
-    function getUrlVar(name){
-        return getUrlVars()[name];
-    }
     function initForm(info){
         $('.infoCard .imgBox img').attr('src',info.imgSrc);
+        if(info.school == ""){
+            $(".schoolChip").css('display','none');
+        }
+        else{
+            $(".school").html(info.school);
+        }
+        if(info.addr == ''){
+            $('addr').css('display','none');
+        }
+        else {
+            $(".addr").html(info.addr);
+        }
         $(".name").html(info.name);
         $(".sex").html(info.sex);
         $(".age").html(info.age+"岁");
         $(".mail").html(info.mail);
         $(".tel").html(info.tel);
-        $(".school").html(info.school);
         $(".department").html(info.depart);
         $(".grade").html(info.grade);
-        $(".addr").html(info.addr);
-        $(".resumeName").html(info.resumePath);
+        var subPath = info.resumePath.split('/');
+        var pathLen = subPath.length;
+        var fileName = subPath[pathLen-1];
+        $(".resumeName").html(fileName);
     }
 });
 
