a6a65fb74b19c61bafe3c0dd0302dcb6f606840c
yuruilee <yuruilee@icloud.com>
2017-01-06 12:09:12 +0800

    互动社区样式修改

--- js/team/topic_detail.js
+++ js/team/topic_detail.js
@@ -11,16 +11,33 @@ $(document).ready(function() {
         data: {'tid': tid},
         success: function (data) {
             team_info.name = data.res.name;
-            team_info.slogan = data.res.slogan;
+            team_info.label = data.res.label;
             team_info.logo = data.res.logo_path;
             team_info.mail = data.res.mail;
             team_info.tel = data.res.tel;
 
             $("#team_logo").attr('src', cur_media + team_info.logo);
             $("#team_name").html(team_info.name);
-            $("#team_slogan").html(team_info.slogan);
             $("#web_site").html(team_info.tel);
             $("#mail_address").html(team_info.mail);
+            var team_label_div=".team_label_div";
+            var topic_detail=".topic_detail";
+            for(i=0;i<team_info.label.length;i++){
+                $(team_label_div).append('<div class="chip team_label">'+team_info.label[i]+'</div>');
+            }
+            var topic_detail_height=parseInt($(topic_detail).css("height").split('px')[0]);
+            var question_div_height=40+topic_detail_height;
+            console.log(question_div_height);
+            var team_label_div_height=parseInt($(team_label_div).css("height").split('px')[0]);
+            var basic_div_height=110+team_label_div_height;
+            console.log(basic_div_height);
+            if(question_div_height<basic_div_height){
+                $(topic_detail).css("height",topic_detail_height+basic_div_height-question_div_height);
+            }
+            else{
+                $(team_label_div).css("height",team_label_div_height-basic_div_height+question_div_height);
+            }
+
         }
     });
 
