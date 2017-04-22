746dbc43e9f244fd9d2ea151ebcf50609ec78cd7
JewelYueng <414608192@qq.com>
2017-04-22 23:55:41 +0800

    更新作品集

--- js/stu/main2.js
+++ js/stu/main2.js
@@ -19,7 +19,18 @@ $(document).ready(function () {
     const work_type_map = ['img', "audio", 'video'];
     let new_work_file = new FormData();
     let new_work_type = -1;
-    let works_arr = [];
+    let works_arr = [{
+        works_id: 0,
+        name: 'heiheihie',
+        duty: 'sa',
+        url: 'http://www.baidu.com',
+        description: 'sa',
+        img: '../../res/imgs/stu/complete.svg',
+        audio: null,
+        video: null
+    }];
+    let current_change_work = new FormData();
+    let current_change_type = -1;
 
     updateSkills(false);
     initWorks();
@@ -98,13 +109,13 @@ $(document).ready(function () {
 
         });
         $('#image_selector').change(() => {
-            changeFile($('#image_selector'), 0);
+            changeFile($('#image_selector'), 0, true);
         });
         $('#video_selector').change(() => {
-            changeFile($('#image_selector'), 1);
+            changeFile($('#video_selector'), 1, true);
         });
         $('#music_selector').change(() => {
-            changeFile($('#image_selector'), 2);
+            changeFile($('#music_selector'), 2, true);
         });
         $('#save-work').click(() => {
             new_work_file.append('stu_id', getUrlVar('stu_id'));
@@ -296,7 +307,7 @@ $(document).ready(function () {
             audio: null,
             video: null
         };
-        new_work[work_type_map[type]]= file_url;
+        new_work[work_type_map[type]] = file_url;
         console.log(new_work);
 
         $.ajax({
@@ -309,7 +320,7 @@ $(document).ready(function () {
                     alert('添加成功');
                     new_work.works_id = data.works_id
                     works_arr.push(new_work);
-                    updateNewWork(new_work,works_arr.length - 1);
+                    updateNewWork(new_work, works_arr.length - 1);
                 } else {
                     if (data.err === "-126") {
                         alert('作品数量已满');
@@ -323,11 +334,19 @@ $(document).ready(function () {
         })
     }
 
-    function changeFile(element, type) {
-        new_work_type = type;
-        new_work_file.append('works', $(element)[0].files[0]);
+    function changeFile(element, type, isAdd) {
+        if (isAdd) {
+            new_work_file.delete('works');
+            new_work_type = type;
+            new_work_file.append('works', $(element)[0].files[0]);
+        } else {
+            current_change_work.delete('works');
+            current_change_type = type;
+            current_change_work.append('works', $(element)[0].files[0])
+        }
         let file_str = $(element).val().split('\\');
         $('#filename').html('上传的文件：' + file_str[file_str.length - 1]);
+
     }
 
     function initWorks() {
@@ -392,7 +411,8 @@ $(document).ready(function () {
                 </video>`
         }
 
-        $('.show-container').append(`<div class="work-content">
+        $('.show-container').append(`<div class="work">
+        <div class="work-content">
             <div class="work-file">
                 ${show_element}
             </div>
@@ -406,18 +426,23 @@ $(document).ready(function () {
                 </div>
                 <div class="work-site">
                     <span class="edit-link"></span>
-                <a class="work-site" href=${work.url}>${work.url}</a></div>
+                <a class="work-link" href=${work.url}>${work.url}</a></div>
             </div>
             <p class="work-p">
                 ${work.description}
             </p>
+        </div>
         </div>`)
         $($('.del-work')[index]).click(() => {
             deleteWork(index)
         })
+        $($('.edit-work')[index]).click(() => {
+            editWork(index)
+        })
 
     }
-    function deleteWork(index){
+
+    function deleteWork(index) {
         $.ajax({
             type: 'POST',
             data: {
@@ -429,7 +454,7 @@ $(document).ready(function () {
             success: (data) => {
                 if (data.err === "0") {
                     alert('删除成功');
-                    $($('.work-content')[index]).remove();
+                    $($('.work')[index]).remove();
                     works_arr.splice(index, 1)
                 } else {
                     console.log(data.msg)
@@ -438,6 +463,178 @@ $(document).ready(function () {
             error: err => console.log(err)
         })
 
+    }
+
+    //测试代码
+    $('.edit-work').click(() => {
+        editWork(0)
+    });
+    function editWork(index) {
+        //    @TODO 显示编辑页面
+        $($('.work-content')[index]).css('display', 'none');
+        $($('.work')[index]).append(`<div class="work-edit-container">
+        <div class="input-field row">
+            <input value=${works_arr[index].name} id="work_name${index}" type="text" class="validate" maxlength="20" >
+            <label for="work_name" class="active">作品名称（20字以内）</label>
+        </div>
+        <div class="input-field row">
+            <input id="work_role${index}" type="text" class="validate" maxlength="20" value=${works_arr[index].duty}>
+            <label for="work_role" class="active">职责介绍（20字以内）</label>
+        </div>
+        <div class="input-field col s12">
+            <textarea class="materialize-textarea" id="work_link${index}" type="text" class="validate"
+                      maxlength="250" >${works_arr[index].url}</textarea>
+            <label for="work_link" class="active">实际作品链接或地址（250字以内）</label>
+        </div>
+        <div>上传作品附件</div>
+        <div class="upload-container">
+            <div class="btn-container">
+                <div class="upload-btn image-selector">
+                    <input type="file" id="image_selector${index}" accept=".jpg,.png,.jpeg.jp3,.jp2">
+                    <label for="image_selector"></label>
+                </div>
+                <p class="hint">(尺寸为800 * 450px)</p>
+            </div>
+            <div class="btn-container">
+                <div class="upload-btn video-selector">
+                    <input type="file" id="video_selector${index}" accept=".mp4,.ogg,.webm">
+                    <label for="video_selector"></label>
+                </div>
+                <p class="hint">(大小在20M以内)</p>
+            </div>
+            <div class="btn-container">
+                <div class="upload-btn music-selector">
+                    <input type="file" id="music_selector${index}" accept=".mp3,.ogg,.mp2">
+                    <label for="music_selector"></label>
+                </div>
+                <p class="hint">(大小在10M以内)</p>
+            </div>
+        </div>
+        <div id="filename"></div>
+        <div class="input-field col s12">
+            <textarea placeholder="作品描述，请输入你在这个作品或者项目中主要做的工作以及相关的成果和绩效是什么，遇到了什么问题然后能够很好的解决，等等"
+                      class="materialize-textarea" id="work_desc${index}" type="text" class="validate" >${works_arr[index].description}</textarea>
+            <label for="work_desc" class="active">作品描述</label>
+        </div>
+        <div class="btn-bar">
+            <button class="btn waves-effect waves-light save-btn">保存</button>
+            <button class="btn waves-effect waves-light cancel-btn">取消</button>`)
+        //    @TODO 上传更新的文件
+        $(`#image_selector${index}`).change(() => {
+            changeFile($(`#image_selector${index}`), 0);
+        });
+        $(`#video_selector${index}`).change(() => {
+            changeFile($(`#video_selector${index}`), 1);
+        });
+        $(`#music_selector${index}`).change(() => {
+            changeFile($(`#music_selector${index}`), 2);
+        });
+        $($('.work .save-btn')[index]).click(() => {
+            current_change_work.append('stu_id', getUrlVar('stu_id'));
+            $.ajax({
+                type: 'post',
+                url: cur_site + 'student/works/upload/',
+                cache: false,
+                processData: false,
+                contentType: false,
+                dataType: 'json',
+                data: current_change_work,
+                success: (data) => {
+                    if (data.err === "0") {
+                        let path = data.path;
+                        updateWork(path, current_change_type, index);
+                    }
+                }
+            })
+        });
+        $($('.work .cancel-btn')[index]).click(() => {
+            $($('.work-content')[index]).css('display', 'block');
+            $($('.work .work-edit-container')[index]).remove()
+        });
+        //    @TODO 访问接口更新文件
+    }
+
+    function updateWork(path, type, index){
+        let new_work = {
+            works_id: works_arr[index].works_id,
+            stu_id: getUrlVar("stu_id"),
+            name: $(`#work_name${index}`).val(),
+            duty: $(`#work_role${index}`).val(),
+            url: $(`#work_link${index}`).val(),
+            description: $(`#work_desc${index}`).val(),
+            img: null,
+            audio: null,
+            video: null
+        };
+        new_work[work_type_map[type]] = path;
+        console.log(new_work);
+
+        $.ajax({
+            type: 'POST',
+            data: new_work,
+            url: cur_site + 'student/info/works/update/',
+            dataType: 'json',
+            success: (data) => {
+                if (data.err === "0") {
+                    alert('更新成功');
+                    works_arr[index] = new_work;
+                    $($('.work-content')[index]).css('display', 'block');
+                    updateWorkDom($('.work-content')[index], new_work)
+                } else {
+                    console.log(data.msg)
+                }
+            },
+            error: err => console.log(err)
+        })
+    }
+    function updateWorkDom(element, work){
+        let show_element;
+        if (work.img) {
+            show_element = `<img src="${cur_media + work.img}" alt="作品图片">`
+        } else if (work.audio) {
+            let type;
+            switch (work.audio.split('.')[1]) {
+                case 'mp3':
+                    type = 'audio/mpeg';
+                    break;
+                case 'ogg':
+                    type = 'audio/ogg';
+                    break;
+            }
+            show_element = `<audio controls="controls">
+                    <source src=${cur_media + work.audio}" type=${type}>
+                    Your browser does not support the audio element.
+                </audio>`
+
+        } else if (work.video) {
+            let type;
+            switch (work.video.split('.')[1]) {
+                case 'mp4':
+                    type = 'audio/mp4';
+                    break;
+                case 'ogg':
+                    type = 'video/ogg';
+                    break;
+                case 'webm':
+                    type = 'video/webm';
+                    break;
+            }
+            show_element = `<video  controls="controls" >
+                    <source src=${cur_media + work.video} type=${type} />
+                    <object data=${cur_media + work.video} width="100%" height="400px">
+                        <embed width="100%" height="400px" src=${cur_media + work.video} />
+                    </object>
+                </video>`
+        }
+        let work_content = $(element).children('.work').children('.work-content');
+        $($(work_content).children('.work-file')).html(show_element);
+        let work_title = $(work_content).children('.work-title');
+        $($(work_title).children('.work-name')).html(work.name);
+        $($(work_title).children('.work-duty')).html(work.duty);
+        let work_link = $(work_title).children('.work-site').children('.work-link');
+        $(work_link).html(work.url);
+        $(work_link).href = work.url;
+        $($(work_content).children('.work-p')).html(work.description);
     }
 });
 
