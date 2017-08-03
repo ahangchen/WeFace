ba813bbb96198123565c087dcfc4c16dbfdf07c6
JewelYueng <414608192@qq.com>
2017-08-04 00:22:02 +0800

    改善了问题五的bug

--- js/team/teamPlayground.js
+++ js/team/teamPlayground.js
@@ -21,19 +21,19 @@ $(function() {
 
     //广告栏的数据处理以及跳转
     $.getJSON('../data/banners.json', function (data) {
-        var lis = $(".imgList li img");
-        var link = $(".imgList li a");
-        for (var i = 0; i < lis.length; i++) {
-            var bannerUrl = data.banner[i].bannerImg;
-            var imgHref = data.banner[i].bannerUrl;
+        let lis = $(".imgList li img");
+        let link = $(".imgList li a");
+        for (let i = 0; i < lis.length; i++) {
+            let bannerUrl = data.banner[i].bannerImg;
+            let imgHref = data.banner[i].bannerUrl;
             console.log(bannerUrl);
             // $(lis[i]).attr("src",cur_media + bannerUrl);
             $(lis[i]).attr("src", bannerUrl);
             $(link[i]).attr("href", imgHref);
         }
     });
-    var curIndexBanner = 0;
-    var imgLen_banner = $(".imgList li").length;
+    let curIndexBanner = 0;
+    let imgLen_banner = $(".imgList li").length;
     $(".preBtn").click(function () {
         curIndexBanner = (curIndexBanner > 0) ? (--curIndexBanner) : (imgLen_banner - 1);
         changeTo(curIndexBanner);
@@ -50,7 +50,7 @@ $(function() {
 
     });
     function changeTo(num) {
-        var goLeft = num * 100;
+        let goLeft = num * 100;
         $(".imgList").animate({left: "-" + goLeft + "%"}, 500);
     }
 
@@ -81,148 +81,185 @@ $(function() {
     }).trigger("mouseleave");
     // －－－－－－－－－－－－－－－热门团队－－－－－－－－－－－－－－－－－－－－－－－－－－－
 // －－－－－－－－－－－－－热门团队初始化－－－－－－－－－－－－－－－－－－－－－－－－－－
-    var wefaceBace_site = "http://wemeet.tech:8080/";
-    var teamlis = $(".teamList .imgFrame img");
-    var teamLink = $(".teamList li a");
-    var teamNames = $(".teamList li .desc");
-    var teamsDesc = $(".teamList li span");
-    var teamLi = $('.teamList li');
-    var curIndexOfTeam = 0;
+    let wefaceBace_site = "http://wemeet.tech:8080/";
+    let teamlis = $(".teamList .imgFrame img");
+    let teamLink = $(".teamList li a");
+    let teamNames = $(".teamList li .desc");
+    let teamsDesc = $(".teamList li span");
+    let teamLi = $('.teamList li');
+    let totalTeam = []
+    let curIndexOfTeam = 0;
     $.getJSON(cur_site + 'team/hot_team/',function(data){
         console.log(data);
-        for (var i = 0; i < data.message.length; i++) {
-            var firstTeamImg = data.message[i].logo_path;
-            var firstTeamName = data.message[i].name;
-            var firstTeamDesc = data.message[i].about;
-            var firstTeamid = data.message[i].id;
-            $(teamlis[i]).attr("src",cur_media + firstTeamImg);
-            $(teamLink[i]).attr("href",wefaceBace_site+'team/team_index.html?tid='+firstTeamid);
-            $(teamNames[i]).html(firstTeamName);
-            $(teamsDesc[i]).html(firstTeamDesc);
-            $(teamLi[i]).attr("id",firstTeamid);
+        totalTeam = data.message
+        for (let i = 0; i < data.message.length; i++) {
+          $(".teamList").append(`<li class="z-depth-2">
+                <a  target="_blank" href="${wefaceBace_site}team/team_index.html?tid=${data.message[i].id}">
+                    <div class="imgDiv">
+                        <div class="imgFrame">
+                        <img src="${cur_media + data.message[i].logo_path}">
+                    </div>
+                    </div>
+                    <div class="desc">${data.message[i].name}</div>
+                    <span>${data.message[i].about}</span></a>
+            </li>`)
+            // let firstTeamImg = data.message[i].logo_path;
+            // let firstTeamName = data.message[i].name;
+            // let firstTeamDesc = data.message[i].about;
+            // let firstTeamid = data.message[i].id;
+            // $(teamlis[i]).attr("src",cur_media + firstTeamImg);
+            // $(teamLink[i]).attr("href",wefaceBace_site+'team/team_index.html?tid='+firstTeamid);
+            // $(teamNames[i]).html(firstTeamName);
+            // $(teamsDesc[i]).html(firstTeamDesc);
+            // $(teamLi[i]).attr("id",firstTeamid);
         }
         curIndexOfTeam += 4;
     });
 
 
 // －－－－－－－－－－－－－－－－－动态加载团队－－－－－－－－－－－－－－－－－－－－－－－－－
-    var triTeamLen = $(teamLi).length;
+    let triTeamLen = $(teamLi).length;
     $(".nextBtnOfTeams").click(function(){
-        var a = $.ajax({
-            type: "get",
-            url: cur_site + "team/hot_team/",
-            dataType: "json",
-            success: function(data){
-                var maxLength = data.message.length;
-                if (curIndexOfTeam == maxLength) {
-                    curIndexOfTeam = 0;
-                }
-                else {
-                    if (triTeamLen < maxLength) {
-                        for (var j = 0; j < 4; j++) {
-                            $(".teamList").append('<li><a  target="_blank"><img ><div class="desc">团队名称</div><span></span></a></li>');
-                        }
-                    }
-                    triTeamLen += 4;
-                    var len = 840 * (triTeamLen / 4);
-                    var lenCss = len + "px";
-                    $(".teamList").css("width", lenCss);
-
-                    teamlis = $(".teamList li img");
-                    teamLink = $(".teamList li a");
-                    teamNames = $(".teamList li div");
-                    teamsDesc = $(".teamList li span");
-                    teamLi = $(".teamList li");
-                    for (var i = curIndexOfTeam; i < curIndexOfTeam + 4; i++) {
-                        var teamImg = data.msg[i].logo_path;
-
-                        var teamName = data.msg[i].name;
-                        var teamDesc = data.msg[i].about;
-                        var teamid = data.msg[i].id;
-                        $(teamlis[i]).attr("src", cur_media + teamImg);
-                        $(teamLink[i]).attr("href",wefaceBace_site+'team/team_index.html?tid='+teamid);
-                        $(teamNames[i]).html(teamName);
-                        $(teamsDesc[i]).html(teamDesc);
-                        $(teamLi[i]).attr("id",teamid);
-                    }
-                }
-                changeToNextTeam(curIndexOfTeam);
-                curIndexOfTeam += 4;
-            },
-            error:function(data){
-                alert(data.msg);
-            },
-            headers: {
-                "Access-Control-Allow-Origin":"*"
-            }
-        });
+        // let a = $.ajax({
+        //     type: "get",
+        //     url: cur_site + "team/hot_team/",
+        //     dataType: "json",
+        //     success: function(data){
+        //         let maxLength = data.message.length;
+        //         if (curIndexOfTeam === maxLength) {
+        //             curIndexOfTeam = 0;
+        //         }
+        //         else {
+        //             if (triTeamLen + 4 < maxLength) {
+        //                 for (let j = 0; j < 4; j++) {
+        //                     $(".teamList").append('<li><a  target="_blank"><img ><div class="desc">团队名称</div><span></span></a></li>');
+        //                 }
+        //             } else {
+        //               for (let j = 0; j < maxLength - triTeamLen; j++) {
+        //                 $(".teamList").append('<li><a  target="_blank"><img ><div class="desc">团队名称</div><span></span></a></li>');
+        //               }
+        //             }
+        //
+        //             triTeamLen += 4;
+        //             let len = 840 * (triTeamLen / 4);
+        //             let lenCss = len + "px";
+        //             $(".teamList").css("width", lenCss);
+        //
+        //             teamlis = $(".teamList li img");
+        //             teamLink = $(".teamList li a");
+        //             teamNames = $(".teamList li div");
+        //             teamsDesc = $(".teamList li span");
+        //             teamLi = $(".teamList li");
+        //             for (let i = curIndexOfTeam; i < curIndexOfTeam + 4; i++) {
+        //               if (teamlis[i]) {
+        //                 let teamImg = data.message[i].logo_path;
+        //
+        //                 let teamName = data.message[i].name;
+        //                 let teamDesc = data.message[i].about;
+        //                 let teamid = data.message[i].id;
+        //                 $(teamlis[i]).attr("src", cur_media + teamImg);
+        //                 $(teamLink[i]).attr("href", wefaceBace_site + 'team/team_index.html?tid=' + teamid);
+        //                 $(teamNames[i]).html(teamName);
+        //                 $(teamsDesc[i]).html(teamDesc);
+        //                 $(teamLi[i]).attr("id", teamid);
+        //               }
+        //             }
+        //         }
+        //         changeToNextTeam(curIndexOfTeam);
+        //         curIndexOfTeam += 4;
+        //     },
+        //     error:function(data){
+        //         alert(data.msg);
+        //     },
+        //     headers: {
+        //         "Access-Control-Allow-Origin":"*"
+        //     }
+        // });
+      if (curIndexOfTeam < totalTeam.length) {
+        let currentLeft = $('.teamList').css('left')
+        console.log(currentLeft)
+        $('.teamList').css('left', currentLeft.split('px')[0] - 852 + 'px')
+        curIndexOfTeam += 4
+      } else {
+        $('.teamList').css('left', 0)
+        curIndexOfTeam = 4
+      }
     });
     $(".preBtnOfTeams").click(function(){
-        var a = $.ajax({
-            type: "get",
-            url: cur_site + "team/hot_team/",
-            dataType: "json",
-            success: function(data){
-                var maxLength = data.message.length;
-
-                if (curIndexOfTeam == 0) {
-                    if(triTeamLen < maxLength) {
-                        for (var j = 0; j < maxLength - triTeamLen; j++) {
-                            $(".teamList").append('<li><a  target="_blank"><img ><div class="desc">团队名称</div><span></span></a></li>');
-                        }
-                        triTeamLen = maxLength;
-                        var len = 840 * (triTeamLen / 3);
-                        var lenCss = len + "px";
-                        $(".teamList").css("width", lenCss);
-                    }
-
-
-                    teamlis = $(".teamList li img");
-                    teamLink = $(".teamList li a");
-                    teamNames = $(".teamList li div");
-                    teamsDesc = $(".teamList li span");
-                    teamLi = $('.teamList li');
-                    for (var i = triTeamLen - 3 ; i < triTeamLen ; i++) {
-                        var teamImg = data.msg[i].logo_path;
-                        var teamName = data.msg[i].name;
-                        var teamDesc = data.msg[i].about;
-                        var teamid = data.msg[i].id;
-                        $(teamlis[i]).attr("src",cur_media + teamImg);
-                        $(teamLink[i]).attr("href",wefaceBace_site+'team/team_index.html?tid='+teamid);
-                        $(teamNames[i]).html(teamName);
-                        $(teamsDesc[i]).html(teamDesc);
-                        $(teamLi[i]).attr("id",teamid);
-                    }
-                    curIndexOfTeam = maxLength - 3;
-                }
-                else{
-                    for (var i = curIndexOfTeam - 3; i < curIndexOfTeam ; i++) {
-                        var teamImg = data.msg[i].logo_path;
-                        var teamName = data.msg[i].name;
-                        var teamDesc = data.msg[i].about;
-                        var teamid = data.msg[i].id;
-                        $(teamlis[i]).attr("src",cur_media + teamImg);
-                        $(teamLink[i]).attr("href",wefaceBace_site+'team/team_index.html?tid='+teamid);
-                        $(teamNames[i]).html(teamName);
-                        $(teamsDesc[i]).html(teamDesc);
-                        $(teamLi[i]).attr("id",teamid);
-                    }
-                    curIndexOfTeam -= 3;
-                }
-                changeToNextTeam(curIndexOfTeam);
-
-            },
-            error:function(data){
-                alert(data.msg);
-            },
-            headers: {
-                "Access-Control-Allow-Origin":"*"
-            }
-        });
+        // let a = $.ajax({
+        //     type: "get",
+        //     url: cur_site + "team/hot_team/",
+        //     dataType: "json",
+        //     success: function(data){
+        //         let maxLength = data.message.length;
+        //
+        //         if (curIndexOfTeam == 0) {
+        //             if(triTeamLen < maxLength) {
+        //                 for (let j = 0; j < maxLength - triTeamLen; j++) {
+        //                     $(".teamList").append('<li><a  target="_blank"><img ><div class="desc">团队名称</div><span></span></a></li>');
+        //                 }
+        //                 triTeamLen = maxLength;
+        //                 let len = 840 * (triTeamLen / 3);
+        //                 let lenCss = len + "px";
+        //                 $(".teamList").css("width", lenCss);
+        //             }
+        //
+        //
+        //             teamlis = $(".teamList li img");
+        //             teamLink = $(".teamList li a");
+        //             teamNames = $(".teamList li div");
+        //             teamsDesc = $(".teamList li span");
+        //             teamLi = $('.teamList li');
+        //             for (let i = triTeamLen - 3 ; i < triTeamLen ; i++) {
+        //                 let teamImg = data.msg[i].logo_path;
+        //                 let teamName = data.msg[i].name;
+        //                 let teamDesc = data.msg[i].about;
+        //                 let teamid = data.msg[i].id;
+        //                 $(teamlis[i]).attr("src",cur_media + teamImg);
+        //                 $(teamLink[i]).attr("href",wefaceBace_site+'team/team_index.html?tid='+teamid);
+        //                 $(teamNames[i]).html(teamName);
+        //                 $(teamsDesc[i]).html(teamDesc);
+        //                 $(teamLi[i]).attr("id",teamid);
+        //             }
+        //             curIndexOfTeam = maxLength - 3;
+        //         }
+        //         else{
+        //             for (let i = curIndexOfTeam - 3; i < curIndexOfTeam ; i++) {
+        //                 let teamImg = data.msg[i].logo_path;
+        //                 let teamName = data.msg[i].name;
+        //                 let teamDesc = data.msg[i].about;
+        //                 let teamid = data.msg[i].id;
+        //                 $(teamlis[i]).attr("src",cur_media + teamImg);
+        //                 $(teamLink[i]).attr("href",wefaceBace_site+'team/team_index.html?tid='+teamid);
+        //                 $(teamNames[i]).html(teamName);
+        //                 $(teamsDesc[i]).html(teamDesc);
+        //                 $(teamLi[i]).attr("id",teamid);
+        //             }
+        //             curIndexOfTeam -= 3;
+        //         }
+        //         changeToNextTeam(curIndexOfTeam);
+        //
+        //     },
+        //     error:function(data){
+        //         alert(data.msg);
+        //     },
+        //     headers: {
+        //         "Access-Control-Allow-Origin":"*"
+        //     }
+        // });
+      if (curIndexOfTeam > 4) {
+        let currentLeft = $('.teamList').css('left')
+        console.log(currentLeft)
+        $('.teamList').css('left', parseInt(currentLeft.split('px')[0]) + 852 + 'px')
+        curIndexOfTeam -= 4
+      } else {
+        $('.teamList').css('left', - 852 * Math.floor(totalTeam.length / 4))
+        curIndexOfTeam = Math.ceil(totalTeam.length / 4) * 4
+      }
     });
     function changeToNextTeam(teamNum){
 
-        var goTeam = teamNum / 3 * 827;
+        let goTeam = teamNum / 3 * 827;
         $(".teamList").animate({left: "-" + goTeam + "px"},500);
     }
 
@@ -277,6 +314,27 @@ $(function() {
         }
 
     }
+  function getUrlVars(){
+    
+    let vars = [], hash;
+    let hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(let i = 0; i < hashes.length; i++)
+    {
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  }
+  
+  //得到指定参数的value
+  function getUrlVar(name){
+    return getUrlVars()[name];
+  }
+  
+  if(getUrlVar('tid')) {
+    $('#team-topic-link').attr('href', `http://wemeet.tech:8080/team/team_index.html?tid=${getUrlVar('tid')}&tab=topic`)
+  }
 
 
 
